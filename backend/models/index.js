222085d22baa1f16c7942106d6c37b501849cd5f
Youssef Lazar <youssef-71150@hotmail.fr>
2021-01-15 14:47:27 +0100

    Mise en place Backend + créations des models + bdd MySQL

---
+++ backend/models/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const dotenv = require ('dotenv') . config ( )
+const db = {};
+
+
+// MySQL DB connection instance using Sequelize & .env variables
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { dialect: "mysql", host: process.env.DB_HOST });
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const app = require('./app');
+const db = require("./models");
+
+
+/*
+ This function returns a valid port, whether supplied as a number or a string
+ */
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '8080');
+app.set('port', port); // Sets on which port the express app should run (the port 3000 by default OR the one specified by the environment if any)
+/*
+This function searches for various server errors and handle them appropriately
+ */
+const errorHandler = (error) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+db.sequelize.sync().then(function() {
+    server.on('error', errorHandler);
+    server.on('listening', () => { // Indicates in the console on which port the server is listening
+        const address = server.address();
+        const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+        console.log('Listening on ' + bind);
+    });
+    server.listen(port); // The server listens on the port 3000 (by default) or on the one specified by the environment if any
+})
