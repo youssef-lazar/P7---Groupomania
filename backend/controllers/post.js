7b19f854abec469ccdc16c633953fd5e9bf9366f
Youssef Lazar <youssef-71150@hotmail.fr>
2021-01-20 15:10:21 +0100

    Installation Frontend + jwt +bcrypt

---
+++ backend/controllers/post.js
@@ -0,0 +1,99 @@
+const Post = require('../models/Post');
+const fs = require('fs');
+
+exports.createPost = (req, res, next) => {
+  const postObject = JSON.parse(req.body.post);
+  delete postObject._id;
+  const post = new Post({
+    ...postObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  post.save()
+    .then(() => res.status(201).json({
+      message: 'Post publié !'
+    }))
+    .catch(error => res.status(400).json({
+      error
+    }));
+};
+
+
+exports.modifyPost = (req, res, next) => {
+  const postObject = req.file ? {
+    ...JSON.parse(req.body.post),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : {
+    ...req.body
+  };
+  Post.updateOne({
+      _id: req.params.id,
+      userId: req.userId
+    }, {
+      ...postObject
+    })
+    .then(() => res.status(200).json({
+      message: 'Post modifié !'
+    }))
+    .catch(error => res.status(400).json({
+      error
+    }));
+};
+
+
+exports.deletePost = (req, res, next) => {
+  Post.findOne({
+      _id: req.params.id,
+      userId: req.userId
+    })
+    .then(post => {
+
+      const filename = post.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Post.deleteOne({
+            _id: req.params.id,
+            userId: req.userId
+          })
+          .then(() => res.status(200).json({
+            message: 'Post supprimé !'
+          }))
+          .catch(error => res.status(400).json({
+            error
+          }));
+      });
+    })
+    .catch(error => res.status(500).json({
+      error
+    }));
+};
+
+
+exports.getOnePost = (req, res, next) => {
+  Post.findOne({
+    _id: req.params.id
+  }).then(
+    (post) => {
+      res.status(200).json(post);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+
+exports.getAllPosts = (req, res, next) => {
+  Post.find().then(
+    (posts) => {
+      res.status(200).json(posts);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
